5cc634255382394526a8a22bdc58d75af1edbd20
Sakib008 <mdshakib1902@gmail.com>
2025-11-08 23:19:01 +0530

    feat : initialized app for the tutor

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-pink-400 text-white font-bold w-screen h-screen flex items-center justify-center text-4xl">
+      This is Your Tutor App
+    </div>
+  );
+}
+
+export default App;
